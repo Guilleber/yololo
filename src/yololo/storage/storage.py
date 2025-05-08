6d636bd7b306fdaed122b36e501bb1c92f6b0b69
Guillaume Le Berre <guillaume.le_berre@depinfonancy.net>
2025-05-07 20:44:40 -0400

    refactor: clean up

--- src/yololo/storage/storage.py
+++ src/yololo/storage/storage.py
@@ -1,5 +1,3 @@
-from typing import Union, Sequence
-
 import chromadb
 from chromadb.api.models import Collection
 
@@ -7,25 +5,16 @@ from yololo.domain.document import Document
 
 
 class DocumentStorage:
-    def __init__(self, collection: Union[Collection, None] = None):
+    def __init__(self, collection: Collection | None = None):
         if collection is None:
             chroma_client = chromadb.Client()
             self.collection = chroma_client.create_collection(name="doc_storage")
         else:
             self.collection = collection
 
-    def add_document(self, document: Document):
-        self.collection.add(
-            documents=[document.content],
-            metadatas=[{"link": document.link,
-                        "title": document.title,
-                        "pub_date": document.pub_date,
-                        "language": document.language,
-                        "source": document.source}],
-            ids=[f"{document.title}___{document.pub_date}"]
-        )
-
-    def add_documents(self, documents: Sequence[Document]):
+    def add_documents(self, documents: list[Document] | Document):
+        if not isinstance(documents, list):
+            documents = [documents]
         self.collection.add(
             documents=[doc.content for doc in documents],
             metadatas=[{"link": doc.link,
@@ -33,12 +22,23 @@ class DocumentStorage:
                         "pub_date": doc.pub_date,
                         "language": doc.language,
                         "source": doc.source} for doc in documents],
-            ids=[f"{doc.title}___{doc.pub_date}" for doc in documents]
+            ids=[doc.title for doc in documents]  # TODO: find a better unique key
         )
 
     def query(self, query: str, result_size: int = 1) -> list[Document]:
         results = self.collection.query(
             query_texts=[query],
+            includes=["documents", "metadatas"],
             n_results=result_size
         )
-        return results
+        documents = []
+        for document, metadata in zip(results.documents[0], results.metadata[0]):
+            documents.append(Document(
+                link=metadata["link"],
+                title=metadata["title"],
+                content=document,
+                pub_date=metadata["pub_date"],
+                language=metadata["language"],
+                source=metadata["source"],
+            ))
+        return documents
