e395d0b45e7978596679a708f9db83822c64400e
david <sirchaos6@gmail.com>
2025-07-23 19:26:24 -0400

    Update openai_api.py

--- src/yololo/llm/openai_api.py
+++ src/yololo/llm/openai_api.py
@@ -1,5 +1,5 @@
 import os
-from typing import Sequence, Tuple
+from typing import Sequence, Tuple, Dict
 
 from openai import OpenAI
 
@@ -14,7 +14,7 @@ class OpenaiApi(ILargeLanguageModel):
         Initializes the openai api class with the due model id and api key
         """
         if api_key_or_path is not None and os.path.isfile(api_key_or_path):
-            self.api_key = api_key_or_path
+            self.api_key = open(api_key_or_path).read()
         elif isinstance(api_key_or_path, str):
             self.api_key = api_key_or_path
         else:
@@ -26,13 +26,17 @@ class OpenaiApi(ILargeLanguageModel):
     def call(self,
              user_prompt: str,
              system_prompt: str = "You are a helpful assistant.",
-             history: Sequence[Tuple[str, str]] | None = None,
+             history: Sequence[Tuple[str, str]] | Sequence[Dict[str, str]] | None = None,
+             **kwargs: Dict[str, str]
              ) -> str:
         history = [] if history is None else history
         conversation_history = []
         for turn in history:
-            conversation_history.append({"role": "user", "content": turn[0]})
-            conversation_history.append({"role": "assistant", "content": turn[1]})
+            if turn is isinstance(turn, Sequence):
+                conversation_history.append({"role": "user", "content": turn[0]})
+                conversation_history.append({"role": "assistant", "content": turn[1]})
+            elif isinstance(turn, Dict):
+                conversation_history.append(turn)
         # prepare the model input
         messages = [
             {"role": "system", "content": system_prompt},
@@ -43,6 +47,7 @@ class OpenaiApi(ILargeLanguageModel):
         completion = self.client.chat.completions.create(
             model=self.model_id,
             messages=messages,
+            **kwargs
         )
         content = completion.choices[0].message.content
         return content
